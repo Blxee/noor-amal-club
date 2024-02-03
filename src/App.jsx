5e49ee38ddf94d46e6050827940d572f5d7db5d9
Blxee <aminedev57@gmail.com>
2024-02-03 14:06:42 +0100

    Added: electron integration

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,42 @@ import './App.css'
 
 function App() {
   return (
-    <>
+    <div className='position-relative d-flex flex-column align-items-center'>
       <h1>Hello World</h1>
-    </>
+      <table className='table table-striped'>
+        <thead>
+          <tr>
+            <th scope='col'>Name</th>
+            <th scope='col'>Registration</th>
+            <th scope='col'>Assurance</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>nam2 bla</td>
+            <td>10-09-2022</td>
+            <td>N/A</td>
+          </tr>
+          <tr>
+            <td>nam2 bla</td>
+            <td>10-09-2022</td>
+            <td>N/A</td>
+          </tr>
+          <tr>
+            <td>nam2 bla</td>
+            <td>10-09-2022</td>
+            <td>N/A</td>
+          </tr>
+          <tr>
+            <td>nam2 bla</td>
+            <td>10-09-2022</td>
+            <td>N/A</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <h1 className='position-absolute top-50 start-50 translate-middle'>Hiiii</h1>
+    </div>
   )
 }
 
